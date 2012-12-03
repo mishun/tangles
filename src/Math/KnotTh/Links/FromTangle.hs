ffb081fd2d244e2db5a7cd2492886308abc75816
Alexander Mishunin <alexander.mishunin@gmail.com>
2012-12-04 00:44:04 +0400

    Tangle doubling

--- src/Math/KnotTh/Links/FromTangle.hs
+++ src/Math/KnotTh/Links/FromTangle.hs
@@ -5,6 +5,7 @@ module Math.KnotTh.Links.FromTangle
 	) where
 
 import Data.Array.IArray
+import Math.Algebra.Group.D4 (D4, ec, (<*>))
 import Math.KnotTh.Crossings.Arbitrary
 import Math.KnotTh.Knotted
 import qualified Math.KnotTh.Links as L
@@ -40,7 +41,32 @@ fromTangleAndStar' withLeg tangle =
 	in L.fromList $ map (\ c -> (map watch $ adjacentDarts c, crossingState c)) $ allCrossings tangle
 
 
-tangleDoubling :: T.Tangle ArbitraryCrossing -> L.Link ArbitraryCrossing
-tangleDoubling tangle = L.fromList $ do
-	c <- allCrossings tangle
-	[]
+tangleDoubling :: (D4 -> D4) -> T.Tangle ArbitraryCrossing -> L.Link ArbitraryCrossing
+tangleDoubling f tangle = L.fromList $ do
+	let atTop d =
+		let c = incidentCrossing d
+		in (2 * crossingIndex c - 1, dartPlace d)
+
+	let atBottom d =
+		let c = incidentCrossing d
+		in (2 * crossingIndex c, 3 - dartPlace d)
+
+	a <- allCrossings tangle
+
+	let top =
+		let pair ab
+			| T.isLeg ba  = atBottom ab
+			| otherwise   = atTop ba
+			where
+				ba = opposite ab
+		in (map pair $ incidentDarts a, crossingState a)
+
+	let bottom =
+		let pair ab
+			| T.isLeg ba  = atTop ab
+			| otherwise   = atBottom ba
+			where
+				ba = opposite ab
+		in (map pair $ reverse $ incidentDarts a, alterCrossingOrientation ((ec <*>) . f) $ crossingState a)
+
+	[top, bottom]
