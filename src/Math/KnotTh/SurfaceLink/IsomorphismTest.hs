9bf174020012a4d31d55b8cf6c54e974e2805785
Alexander Mishunin <alexander.mishunin@gmail.com>
2013-10-28 20:18:46 +0400

    Virtual generator

--- src/Math/KnotTh/SurfaceLink/IsomorphismTest.hs
+++ src/Math/KnotTh/SurfaceLink/IsomorphismTest.hs
@@ -4,7 +4,7 @@ module Math.KnotTh.SurfaceLink.IsomorphismTest
     ) where
 
 import Prelude hiding (head, tail)
-import Data.Bits (shiftL)
+import Data.Bits ((.&.), shiftL)
 import Data.Function (fix)
 import Data.STRef (newSTRef, readSTRef, writeSTRef)
 import Data.Array.Base (unsafeRead, unsafeWrite)
@@ -12,7 +12,7 @@ import Data.Array.Unboxed (UArray)
 import Data.Array.ST (STArray, STUArray, runSTUArray, newArray, newArray_)
 import Control.Monad.ST (ST)
 import Control.Monad (when, void)
-import Math.Algebra.RotationDirection (RotationDirection, ccw, cw)
+import Math.Algebra.RotationDirection (RotationDirection, ccw, cw, directionSign)
 import Math.KnotTh.SurfaceLink
 
 
@@ -28,6 +28,7 @@ codeWithDirection !dir !start = runSTUArray $ do
     let n = numberOfCrossings link
 
     index <- newArray (0, n) 0 :: ST s (STUArray s Int Int)
+    incoming <- newArray (0, n) 0 :: ST s (STUArray s Int Int)
     queue <- newArray_ (0, n - 1) :: ST s (STArray s Int (Dart SurfaceLink ct))
     free <- newSTRef 1
 
@@ -36,32 +37,35 @@ codeWithDirection !dir !start = runSTUArray $ do
             let u = crossingIndex $! incidentCrossing d
             ux <- unsafeRead index u
             if ux > 0
-                then return $! ux
+                then do
+                    up <- unsafeRead incoming u
+                    return $! (ux `shiftL` 2) + (((dartPlace d - up) * directionSign dir) .&. 3)
                 else do
                     nf <- readSTRef free
                     writeSTRef free $! nf + 1
                     unsafeWrite index u nf
+                    unsafeWrite incoming u (dartPlace d)
                     unsafeWrite queue (nf - 1) d
-                    return $! nf
+                    return $! nf `shiftL` 2
 
-        {-# INLINE lookAndAdd #-}
-        lookAndAdd !d !s = do
-            !c <- look d
-            return $! c + s `shiftL` 7
-
-    rc <- newArray (0, 2 * n) 0 :: ST s (STUArray s Int Int)
+    rc <- newArray (0, 6 * n) 0 :: ST s (STUArray s Int Int)
     unsafeWrite rc 0 $! numberOfFreeLoops link
+
+    let {-# INLINE lookAndWrite #-}
+        lookAndWrite !d !offset = do
+            look d >>= unsafeWrite rc offset
+            return $! offset + 1
 
     void $ look start
     flip fix 0 $ \ bfs !head -> do
         tail <- readSTRef free
         when (head < tail - 1) $ do
             input <- unsafeRead queue head
-            !nb <- foldMAdjacentDartsFrom input dir lookAndAdd 0
+            void $ foldMAdjacentDartsFrom input dir lookAndWrite (6 * head + 3)
             case crossingCode dir input of
                 (# be, le #) -> do
-                    unsafeWrite rc (1 + 2 * head) be
-                    unsafeWrite rc (2 + 2 * head) $! le + nb `shiftL` 3
+                    unsafeWrite rc (6 * head + 1) be
+                    unsafeWrite rc (6 * head + 2) le
             bfs $! head + 1
 
     fix $ \ _ -> do
--- test/VirtualBenchmark.hs
+++ test/VirtualBenchmark.hs
@@ -11,7 +11,9 @@ import Text.Printf
 import Math.Algebra.Group.Dn (rotationPeriod, hasReflectionPart)
 import Math.KnotTh.Crossings.SubTangle
 import Math.Combinatorics.ChordDiagrams.Generator
-import Math.KnotTh.Tangle
+import Math.KnotTh.Tangle.Projection
+import Math.KnotTh.Tangle.BorderIncremental.IncrementalGluing
+import Math.KnotTh.Tangle.BorderIncremental.SimpleTypes
 import Math.KnotTh.Tangle.BorderIncremental.FlypeGenerator (generateFlypeEquivalentDecomposition)
 import Math.KnotTh.SurfaceLink
 import Math.KnotTh.SurfaceLink.FromTangle (fromTangleAndStarByOffset)
@@ -33,7 +35,7 @@ main = do
     table <- newIORef M.empty
     let yield link = do
             let g = (2 - eulerChar link) `div` 2
-                n = numberOfCrossingsAfterSubstitution link
+                n = numberOfCrossings link --numberOfCrossingsAfterSubstitution link
             modifyIORef' table (M.insertWith' (+) (n, g) 1)
 
     lookupSet <- newIORef S.empty
@@ -45,12 +47,13 @@ main = do
                     modifyIORef' lookupSet (S.insert arr)
                     return True
 
-    let maxN = 5
+    let maxN = 7
 
-    generateFlypeEquivalentDecomposition maxN $ \ !tangle !tangleSymmetry -> do
+    --generateFlypeEquivalentDecomposition maxN $ \ !tangle !tangleSymmetry -> do
+    simpleIncrementalGenerator templateProjectionType [ProjectionCrossing] maxN $ \ !tangle !tangleSymmetry -> do
         let l = numberOfLegs tangle
         forM_ (generateCD $ l `div` 2) $ \ (!bucket, (bucketMirror, bucketPeriod)) -> do
-            let period = gcd bucketPeriod $ rotationPeriod tangleSymmetry
+            let period = gcd bucketPeriod (rotationPeriod tangleSymmetry)
                 mirror = not bucketMirror && not (hasReflectionPart tangleSymmetry)
 
             forM_ [(r, m) | r <- [0 .. period - 1], m <- if mirror then [False, True] else [False]] $ \ (r, m) -> do
