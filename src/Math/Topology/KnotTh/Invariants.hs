754b12a741526ced374d50d548f4654b5daf2337
Alexander Mishunin <alexander.mishunin@gmail.com>
2013-12-04 16:43:37 +0400

    Group by invariant

--- src/Math/Topology/KnotTh/Invariants.hs
+++ src/Math/Topology/KnotTh/Invariants.hs
@@ -1,9 +1,21 @@
 module Math.Topology.KnotTh.Invariants
     ( module X
+    , groupByInvariant
     ) where
 
+import Data.Function (on)
+import Data.Ord (comparing)
+import Data.List (groupBy, sortBy)
 import Math.Topology.KnotTh.Invariants.LinkingNumber as X
 import Math.Topology.KnotTh.Invariants.JonesPolynomial as X
 import Math.Topology.KnotTh.Invariants.KauffmanFPolynomial as X
 import Math.Topology.KnotTh.Invariants.KauffmanXPolynomial as X
 import Math.Topology.KnotTh.Invariants.HomflyPolynomial as X
+
+
+groupByInvariant :: (Ord x) => (a -> x) -> [a] -> [(x, [a])]
+groupByInvariant f =
+    map (\ l@((x, _) : _) -> (x, map snd l)) .
+        groupBy (on (==) fst) .
+            sortBy (comparing fst) .
+                map (\ x -> (f x, x))
