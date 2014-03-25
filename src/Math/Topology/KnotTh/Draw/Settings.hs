d4a0a868b7623c01ff79ee76b011276aa3d6c1bc
Alexander Mishunin <alexander.mishunin@gmail.com>
2014-03-25 06:03:28 +0400

    Indent

--- src/Math/Topology/KnotTh/Draw/Settings.hs
+++ src/Math/Topology/KnotTh/Draw/Settings.hs
@@ -10,27 +10,29 @@ import Control.Monad (forM_)
 import Diagrams.Prelude
 
 
-data DrawKnotSettings = DrawKnotSettings
-    { threadWidth      :: Double
-    , threadColour     :: Colour Double
-    , borderWidth      :: Double
-    , borderColour     :: Colour Double
-    , borderDashing    :: [Double]
-    , backgroundColour :: Colour Double
-    , endpointsRadius  :: Double
-    }
+data DrawKnotSettings =
+    DrawKnotSettings
+        { threadWidth      :: Double
+        , threadColour     :: Colour Double
+        , borderWidth      :: Double
+        , borderColour     :: Colour Double
+        , borderDashing    :: [Double]
+        , backgroundColour :: Colour Double
+        , endpointsRadius  :: Double
+        }
 
 
 defaultDraw :: DrawKnotSettings
-defaultDraw = DrawKnotSettings
-    { threadWidth      = 0.03
-    , threadColour     = black
-    , borderWidth      = 0.02
-    , borderColour     = black
-    , borderDashing    = [0.08, 0.08]
-    , backgroundColour = white
-    , endpointsRadius  = 0.04
-    }
+defaultDraw =
+    DrawKnotSettings
+        { threadWidth      = 0.03
+        , threadColour     = black
+        , borderWidth      = 0.02
+        , borderColour     = black
+        , borderDashing    = [0.08, 0.08]
+        , backgroundColour = white
+        , endpointsRadius  = 0.04
+        }
 
 
 styleBorder :: (HasStyle c) => DrawKnotSettings -> c -> c
