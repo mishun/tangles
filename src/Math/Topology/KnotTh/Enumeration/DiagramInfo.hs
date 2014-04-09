d2e4eac7d3a6bdbb3f0b3678bcd870f5e4e5ce2f
Alexander Mishunin <alexander.mishunin@gmail.com>
2014-04-09 07:30:09 +0400

    Fix for equvalence classes generator

--- src/Math/Topology/KnotTh/Enumeration/DiagramInfo.hs
+++ src/Math/Topology/KnotTh/Enumeration/DiagramInfo.hs
@@ -6,6 +6,6 @@ import Math.Topology.KnotTh.Knotted
 
 
 class DiagramInfo info where
-    merge          :: (KnottedWithPrimeTest k) => info (k ct) -> info (k ct) -> info (k ct)
-    wrap           :: (KnottedWithPrimeTest k) => k ct -> info (k ct)
-    representative :: (KnottedWithPrimeTest k) => info (k ct) -> k ct
+    merge          :: (Crossing a, KnottedWithPrimeTest k) => info (k a) -> info (k a) -> info (k a)
+    wrap           :: (Crossing a, KnottedWithPrimeTest k) => k a -> info (k a)
+    representative :: (KnottedWithPrimeTest k) => info (k a) -> k a
--- src/Math/Topology/KnotTh/Enumeration/DiagramInfo/AllDiagramsInfo.hs
+++ src/Math/Topology/KnotTh/Enumeration/DiagramInfo/AllDiagramsInfo.hs
@@ -4,19 +4,31 @@ module Math.Topology.KnotTh.Enumeration.DiagramInfo.AllDiagramsInfo
     , allDiagrams
     ) where
 
+import qualified Data.Vector.Unboxed as UV
+import Math.Topology.KnotTh.Knotted
 import Math.Topology.KnotTh.Enumeration.DiagramInfo as X
 
 
-newtype AllDiagramsInfo k = AllDiagramsInfo [k] deriving (Show)
+newtype AllDiagramsInfo k = AllDiagramsInfo [(UV.Vector Int, k)] deriving (Show)
 
 
 instance DiagramInfo AllDiagramsInfo where
-    merge (AllDiagramsInfo a) (AllDiagramsInfo b) = AllDiagramsInfo $ a ++ b
+    merge (AllDiagramsInfo list1) (AllDiagramsInfo list2) =
+        let merge' [] b = b
+            merge' a [] = a
+            merge' la@(a@(ia, _) : ta) lb@(b@(ib, _) : tb) =
+                case compare ia ib of
+                    LT -> a : merge' ta lb
+                    GT -> b : merge' la tb
+                    EQ -> merge' la tb
+        in AllDiagramsInfo $ merge' list1 list2
 
-    wrap x = AllDiagramsInfo [x]
+    wrap x = AllDiagramsInfo [(unrootedHomeomorphismInvariant x, x)]
 
-    representative (AllDiagramsInfo l) = head l
+    representative (AllDiagramsInfo l) =
+        let (_, r) : _ = l
+        in r
 
 
 allDiagrams :: AllDiagramsInfo k -> [k]
-allDiagrams (AllDiagramsInfo l) = l
+allDiagrams (AllDiagramsInfo l) = map snd l
