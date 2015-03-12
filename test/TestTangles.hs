0f366bdd7118590262753366f058b0cd41d2ac46
Alexander Mishunin <alexander.mishunin@gmail.com>
2015-03-12 23:22:21 +0300

    Tests

--- test/TestTangles.hs
+++ test/TestTangles.hs
@@ -1,8 +1,23 @@
 module Main (main) where
 
 import Test.Framework (defaultMain)
-import TestUtil.TestCollection
+import qualified Math.Combinatorics.ChordDiagram.Test
+import qualified Math.Combinatorics.Strings.Test
+import qualified Math.Topology.KnotTh.EmbeddedLink.Test
+import qualified Math.Topology.KnotTh.Enumeration.Applied.Test
+import qualified Math.Topology.KnotTh.Invariants.Test
+import qualified Math.Topology.KnotTh.Tabulation.Test
+import qualified Math.Topology.KnotTh.Tangle.Test
 
 
 main :: IO ()
-main = defaultMain tests
+main =
+    defaultMain
+        [ Math.Combinatorics.Strings.Test.test
+        , Math.Topology.KnotTh.EmbeddedLink.Test.test
+        , Math.Topology.KnotTh.Tangle.Test.test
+        , Math.Topology.KnotTh.Invariants.Test.test
+        , Math.Combinatorics.ChordDiagram.Test.test
+        , Math.Topology.KnotTh.Tabulation.Test.test
+        , Math.Topology.KnotTh.Enumeration.Applied.Test.test
+        ]
